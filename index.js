e02c2dfcb0ef9ad40a6d568096bb8f66d2256653
Jerome Paradis <jerome@buyosphere.com>
2012-01-19 08:58:30 -0800

    Use red-socket.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express= require('express');
 
 var socket_auth = require('../socket-auth')
-   ,Api = require('./lib/api');
+   ,Api = require('./lib/api')
+   ,RedSocket = require('../red-socket');
 
 /**
  * Export the wrapper.
@@ -13,11 +14,12 @@ function Notify(app, io, options) {
 	app.configure(function(){
   		app.use(express.bodyParser());
 	});
+	this.rsr = RedSocket(io);
 	this.auth_plugin = (options && options.auth_plugin) || "django-auth";
 	this.api_key = (options && options.auth_plugin) || null;
 	auth = socket_auth(io, this.auth_plugin, options);
 
-	var messages_api = Api.MessagesApi(io);
+	var messages_api = Api.MessagesApi(this.rsr);
 
 	//if (app.resource) {
 	app.resource('api/messages', messages_api, { format: 'json' });
