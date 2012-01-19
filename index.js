8f4ee61bea1be289d6f844d93520984f1dcd0ea0
Jerome Paradis <jerome@buyosphere.com>
2012-01-18 16:06:42 -0800

    Blank API.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var socket_auth = require('../socket-auth'),
-  , Resource = require('express-resource');
+var socket_auth = require('../socket-auth');
 
 /**
  * Export the wrapper.
@@ -9,10 +8,25 @@ exports = module.exports = Notify;
 
 function Notify(app, io, options) {
 	this.auth_plugin = (options && options.auth_plugin) || "django-auth";
-	this.api_key: (options && options.auth_plugin) || null;
+	this.api_key = (options && options.auth_plugin) || null;
 	auth = socket_auth(io, this.auth_plugin, options);
 
-	//TODO: app.resource('forums', require('./forum'));
+	var messages_api = function() {
+		this.index = function(req, res){
+  			res.send('OK');
+		};
+		this.create = function(req, res){
+  			res.send('OK');
+		};
+
+		return this;
+	}();
+
+
+	//if (app.resource) {
+	app.resource('messages', messages_api);
+
+	//}
 
 	return auth;
 }
