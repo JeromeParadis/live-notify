aef00c48b46d1018ceae6bb982c1792c648755c2
Jerome Paradis <jerome@buyosphere.com>
2012-01-19 08:50:32 -0800

    Added basic API to broadcast messages.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var socket_auth = require('../socket-auth');
+var express= require('express');
+
+var socket_auth = require('../socket-auth')
+   ,Api = require('./lib/api');
 
 /**
  * Export the wrapper.
@@ -7,24 +10,17 @@ var socket_auth = require('../socket-auth');
 exports = module.exports = Notify;
 
 function Notify(app, io, options) {
+	app.configure(function(){
+  		app.use(express.bodyParser());
+	});
 	this.auth_plugin = (options && options.auth_plugin) || "django-auth";
 	this.api_key = (options && options.auth_plugin) || null;
 	auth = socket_auth(io, this.auth_plugin, options);
 
-	var messages_api = function() {
-		this.index = function(req, res){
-  			res.send('OK');
-		};
-		this.create = function(req, res){
-  			res.send('OK');
-		};
-
-		return this;
-	}();
-
+	var messages_api = Api.MessagesApi(io);
 
 	//if (app.resource) {
-	app.resource('messages', messages_api);
+	app.resource('api/messages', messages_api, { format: 'json' });
 
 	//}
 
