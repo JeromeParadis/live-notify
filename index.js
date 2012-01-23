a8188a95ee260987aa81d2d34ab0715670ab2d9d
Jerome Paradis <jerome@buyosphere.com>
2012-01-23 12:09:42 -0800

    Added Backbone views to manage notifyier widget.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function Notify(app, io, options) {
 	models.Backbone.initServer(app);
 	this.redis_prefix = '';
 	this.models = models;
-	this.rsr = RedSocket(io);
+	this.rsr = RedSocket(io,{debug: true});
 	this.auth_plugin = (options && options.auth_plugin) || "django-auth";
 	this.api_key = (options && options.auth_plugin) || null;
 	this.transport = new Transport(this);
@@ -40,14 +40,22 @@ function Notify(app, io, options) {
 	// Define auth acknowledge
 	// -----------------------
 	var self = this;
-	this.socket_auth = new SocketAuth(io, this.auth_plugin, options, function(socketid,session) {
-		self.rsr.r_send_user(socketid,"ready","authenticated");
+	this.socket_auth = new SocketAuth(io, this.auth_plugin, options, function(socket,session) {
+		self.rsr.r_send_user(socket.id,"ready","authenticated");
 		if (session && session.user_id) {
 			var notifications = new Notifications.UserNotifications(session.user_id,self);
-			notifications.get_notifications(1,20,function(err,notes) {
-				self.rsr.r_send_user(socketid,"notes-init",notes);
+			notifications.get_notifications_count(function(err,notes) {
+				self.rsr.r_send_user(socket.id,"notes-count",notes);
 			});
-		}	
+			socket.on("get_initial_notes",function() {
+				console.log("get_initial_notes()")
+				notifications.get_notifications(1,20,function(err,notes) {
+					console.log("Received notes!")
+					self.rsr.r_send_user(socket.id,"notes-init",notes);
+				});
+			});
+		}
+
 	});
 
 	// API views
@@ -55,12 +63,12 @@ function Notify(app, io, options) {
 	//if (app.resource) {
 	app.resource('api/message', Api.MessageApi(this), { format: 'json' });
 	app.resource('api/events', Api.EventsApi(this), { format: 'json' });
-	app.get('/js/*', function(req, res){
-	    res.sendfile(__dirname + '/public'+req.url);
+	app.get('/js/live-notify.js', function(req, res){
+	    res.sendfile(__dirname + '/public/js/live-notify.js');
 	});
-	app.get('/models/*', function(req, res){
+	app.get('/models/models.js', function(req, res){
 	    console.log("req.url",req.url)
-	    res.sendfile(__dirname + req.url);
+	    res.sendfile(__dirname + '/models/models.js');
 	});
 
 	//}
--- public/js/live-notify.js
+++ public/js/live-notify.js
@@ -1,7 +1,9 @@
   var LiveNotify = function(url,sessionid,options) {
     this.url = url;
     this.sessionid = sessionid;
+    this.wrapper_selector = (options && options.wrapper_selector) || '#notify-wrapper';
     this.icon_selector = (options && options.icon_selector) || '#notify-icon';
+    this.notifier = null;
     var self = this;
 
     if (!sessionid) {
@@ -32,15 +34,25 @@
     // ----------------------------------
     socket.on('ready', function (data) {
       console.log("Ready!");
-      $(self.icon_selector).removeAttr("disabled").removeClass("s-disabled"); 
+      self.notifier = new Notifier();
+    });
+
+    // Initial notes on page load
+    // ----------------------------------
+    socket.on('notes-count', function (data) {
+      console.log("Count!");
+      var nb_notes = data || 0;
+      self.notifier.update_count(nb_notes);
     });
 
     // Initial notes on page load
     // ----------------------------------
     socket.on('notes-init', function (data) {
       console.log("Init!");
+
       var nb_notes = (data && data.length) || 0;
-      $(self.icon_selector).addClass("button-red").val(nb_notes);
+      self.notifier.update_count(nb_notes);
+      self.notifier.show_notes();
     });
 
     // Direct message
@@ -63,6 +75,70 @@
     require([this.url + 'models/models.js'],function() {
       //alert(models.Event)
     });
+
+    // Views
+    // ----------------------------------
+
+    // Notifier View
+    // ----------------------------------
+    Notifier = Backbone.View.extend({
+      tagName: "div",
+      className: "notify-notifier",
+      nb_notes: 0,
+      wrapper_selector: self.wrapper_selector,
+      icon_selector: self.icon_selector,
+      events: {
+        "click .notify-icon":      "click_expand",
+        },
+      initialize: function() {
+        $(this.el).html("");
+        this.render();
+        $(self.wrapper_selector).html(this.el);
+        $(this.icon_selector).removeAttr("disabled").removeClass("s-disabled"); 
+        console.log("View initialized");
+      },
+      template: $.template("#template-notify-icon"),
+      render: function() {
+        $(this.el).html(this.template());
+        return this;
+      },
+      update_count: function(nb) {
+        this.nb_notes = nb;
+        this.render_count();
+        return this;
+      },
+      click_expand: function() {
+        console.log("click");
+        socket.emit('get_initial_notes');
+        return this;
+      },
+      show_notes: function() {
+        alert('We got notes!');
+
+        return this;
+      },
+      render_count: function() {
+        if (this.nb_notes > 0) 
+          $(this.icon_selector).addClass("button-red");
+        else
+          $(this.icon_selector).removeClass("button-red");
+        $(this.icon_selector).val(this.nb_notes); 
+
+        return this;
+      }
+    });
+
+    // Notifier View
+    // ----------------------------------
+    NotificationSummary = Backbone.View.extend({
+      tagName: "div",
+      className: "notify-summary",
+      nb_notes: 0,
+      events: {
+        }
+    });
+
+
 
 
     return this;
